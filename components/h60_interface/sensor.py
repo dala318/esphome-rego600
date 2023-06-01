79480050029a8a87ac45a46a32ae1e1d9780ca5e
Daniel Vikström <daniel@vikstrom.name>
2023-06-01 11:38:45 +0200

    Keeping konfig parameters in dictionary

--- components/h60_interface/sensor.py
+++ components/h60_interface/sensor.py
@@ -19,13 +19,20 @@ Sensor = sensor_ns.class_('Sensor', sensor.Sensor)
 DEFAULT_UPDATE_INTERVAL = "5s"
 
 h60_ns = cg.esphome_ns.namespace("h60_interface")
-CONF_POWER = "power"
-SensorPower = h60_ns.class_("SensorPower", sensor.Sensor, cg.PollingComponent)
-CONF_RETURN_TEMP = "return_temp"
-SensorReturnTemp = h60_ns.class_("SensorReturnTemp", sensor.Sensor, cg.PollingComponent)
+# CONF_POWER = "power"
+# CONF_RETURN_TEMP = "return_temp"
+CONF_DICT = {
+    "power": h60_ns.class_("SensorPower", sensor.Sensor, cg.PollingComponent),
+    "return_temp": h60_ns.class_("SensorReturnTemp", sensor.Sensor, cg.PollingComponent)
+}
+CONF_DICT = {
+    cv.Optional("power"): sensor.sensor_schema(h60_ns.class_("SensorPower", sensor.Sensor, cg.PollingComponent)).extend(cv.COMPONENT_SCHEMA),
+    cv.Optional("return_temp"): sensor.sensor_schema(h60_ns.class_("SensorReturnTemp", sensor.Sensor, cg.PollingComponent)).extend(cv.COMPONENT_SCHEMA),
+}
+# SensorPower = h60_ns.class_("SensorPower", sensor.Sensor, cg.PollingComponent)
+# SensorReturnTemp = h60_ns.class_("SensorReturnTemp", sensor.Sensor, cg.PollingComponent)
 
 # CONF_SENSOR_PARAMETERS = ["power", "return_temp"]
-
 # CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
 #     cv.GenerateID(): cv.declare_id(Sensor),
 #     cv.GenerateID(CONF_HUB_ID): cv.use_id(H60InterfaceComponent),
@@ -35,12 +42,15 @@ SensorReturnTemp = h60_ns.class_("SensorReturnTemp", sensor.Sensor, cg.PollingCo
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_HUB_ID): cv.use_id(H60InterfaceComponent),
-        cv.Optional(CONF_POWER): sensor.sensor_schema(SensorPower)
-            .extend(cv.COMPONENT_SCHEMA),
-        cv.Optional(CONF_RETURN_TEMP): sensor.sensor_schema(SensorReturnTemp)
-            .extend(cv.COMPONENT_SCHEMA),
+        # cv.Optional(CONF_POWER): sensor.sensor_schema(SensorPower)
+        #     .extend(cv.COMPONENT_SCHEMA),
+        # cv.Optional(CONF_RETURN_TEMP): sensor.sensor_schema(SensorReturnTemp)
+        #     .extend(cv.COMPONENT_SCHEMA),
     }
-)# .extend(cv.COMPONENT_SCHEMA)
+# )# .extend(cv.COMPONENT_SCHEMA)
+).extend(CONF_DICT)
+# for key, val in CONF_DICT.items():
+#     CONFIG_SCHEMA.extend({cv.Optional(key): sensor.sensor_schema(val).extend(cv.COMPONENT_SCHEMA)})
 
 async def setup_conf(paren, config, key):
     if key in config:
@@ -48,13 +58,16 @@ async def setup_conf(paren, config, key):
         var = await sensor.new_sensor(conf)
         # await sensor.register_sensor(var, conf)
         await cg.register_component(var, conf)
-        cg.add(paren.register_sensor(key, var))
+        cg.add(paren.register_sensor(str(key), var))
 
 
 async def to_code(config):
     paren = await cg.get_variable(config[CONF_HUB_ID])
-    await setup_conf(paren, config, CONF_POWER)
-    await setup_conf(paren, config, CONF_RETURN_TEMP)
+    for key in CONF_DICT.keys():
+        await setup_conf(paren, config, key)
+
+    # await setup_conf(paren, config, CONF_POWER)
+    # await setup_conf(paren, config, CONF_RETURN_TEMP)
 
 
 # async def to_code(config):
